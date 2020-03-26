319c668c0dc3ddefb77f3b0f785f3202ffb52a71
Aaron1sharp <podaroktebe2014@gmail.com>
2020-03-26 20:38:19 +0600

    end scroll,falling and up platform

---
+++ Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+public class FallingPlatform : MonoBehaviour
+{
+    Rigidbody2D _rigidbody2D;
+    Vector2 _currentPosition;
+    bool _moveingBack;
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _currentPosition = transform.position;
+    }
+    private void Update()
+    {
+        if (_moveingBack == true)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, _currentPosition, 20f * Time.deltaTime);
+        }
+
+        if (transform.position.y == _currentPosition.y)
+        {
+            _moveingBack = false;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ((collision.gameObject.name.Equals("Player") || collision.gameObject.CompareTag("ground")) && _moveingBack == false) 
+        {
+            Invoke("FallPlatform", 2f);
+        }   
+    }
+        void FallPlatform()
+        {
+            _rigidbody2D.isKinematic = false;
+            Invoke("BackPlatform", 2f);
+        }
+
+        void BackPlatform()
+        {
+            _rigidbody2D.velocity = Vector2.zero;
+            _rigidbody2D.isKinematic = true;
+            _moveingBack = true;
+        }
+}
--- Assets/Scripts/Scrolling.cs
+++ Assets/Scripts/Scrolling.cs
@@ -1,61 +1,77 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Scrolling : MonoBehaviour
 {
-    public float BackGroundSize;
+    public bool _scrolling, _paralax;
+    public float _backGroundSize, _paralaxSpeed;
  
-    private Transform cameraTransform;
-    private Transform[] layers;
-    private float viewZone = 11;
-    private int leftIndex, rightIndex;
-    private void Start()
+    Transform CameraTransform;
+    Transform[] Layers;
+    float _viewZone = 11, _lastCameraX;
+    int _leftIndex, _rightIndex;
+
+    void Start()
     {
-        cameraTransform = Camera.main.transform;
-        layers = new Transform[transform.childCount];
+
+        CameraTransform = Camera.main.transform;
+        _lastCameraX = CameraTransform.position.x;
+        Layers = new Transform[transform.childCount];
+
         for (int i = 0; i < transform.childCount; i++)
         {
-            layers[i] = transform.GetChild(i);
+            Layers[i] = transform.GetChild(i);
         }
-        leftIndex = 0;
-        rightIndex = layers.Length - 1;
+
+        _leftIndex = 0;
+        _rightIndex = Layers.Length - 1;
+
+    }
+    void Update()
+    {
+
+        if (_paralax)
+        {
+            float deltaX = CameraTransform.position.x - _lastCameraX;
+            transform.position += Vector3.right * (deltaX * _paralaxSpeed);
+        }
+
+        _lastCameraX = CameraTransform.position.x;
+
+        if (_scrolling)
+        {
+            if (CameraTransform.position.x < (Layers[_leftIndex].transform.position.x + _viewZone))
+            {
+                ScrollLeft();
+            }
+            if (CameraTransform.position.x > (Layers[_rightIndex].transform.position.x - _viewZone))
+            {
+                ScrollRight();
+            }
+        }
+
     }
 
-    private void Update()
+    void ScrollLeft()
     {
-        if (cameraTransform.position.x < (layers[leftIndex].transform.position.x + viewZone)) 
-        {
-            ScrollLeft();
-        }
-        if (cameraTransform.position.x > (layers[rightIndex].transform.position.x - viewZone))
-        {
-            ScrollRight();
-        }
+
+        Layers[_rightIndex].position = Vector3.right * (Layers[_leftIndex].position.x - _backGroundSize);
+        _leftIndex = _rightIndex;
+        _rightIndex--;
+
+        if (_rightIndex < 0)
+            _rightIndex = Layers.Length - 1;  
+
     }
 
-    private void ScrollLeft()
+    void ScrollRight()
     {
-        int lastRight = rightIndex;
-        layers[rightIndex].position = Vector3.right * (layers[leftIndex].position.x - BackGroundSize);
-        leftIndex = rightIndex;
-        rightIndex--;
 
-        if (rightIndex < 0)
-        {
-            rightIndex = layers.Length - 1;
-        }
-    }
-    private void ScrollRight()
-    {
-        int lastLeft = leftIndex;
-        layers[leftIndex].position = Vector3.right * (layers[rightIndex].position.x + BackGroundSize);
-        rightIndex = leftIndex;
-        leftIndex++;
+        Layers[_leftIndex].position = Vector3.right * (Layers[_rightIndex].position.x + _backGroundSize);
+        _rightIndex = _leftIndex;
+        _leftIndex++;
 
-        if (leftIndex == layers.Length)
-        {
-            leftIndex = 0;
-        }
+        if (_leftIndex == Layers.Length)
+            _leftIndex = 0;
+
     }
 }
